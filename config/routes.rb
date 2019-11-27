5c1eccc9e2fff2c87986eee16c7a0c0ba2eeb33f
Tripple-A <biodun9@gmail.com>
2019-11-27 17:13:12 +0100

    added the sessions new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/sign_in' to: 'sessions#new'
-  post '/sign_in' to: 'sessions#create'
+  root 'sessions#new'
+  #post '/login' to: 'sessions#create'
 end
